e7421e7c759d52abcfe0733c70c6c8d854b77840
Gururaj <17214716+rajguru827@users.noreply.github.com>
2019-07-07 22:09:10 +0530

    Added auth, sign-in & sign-up modules and ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '', loadChildren: () => import('./user/user.module').then(mod => mod.UserModule)
+  },
+  {
+    path: 'auth', loadChildren: () => import('./auth/auth.module').then(mod => mod.AuthModule)
   }
 ];
 
