305b207e6a8cc9fad1a93f2058816ad1945710d1
Gururaj <17214716+rajguru827@users.noreply.github.com>
2020-03-25 20:17:48 +0530

    chart.js and added bar chart in dashboard

--- src/app/user/dashboard/dashboard.component.ts
+++ src/app/user/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
+import { TransactionService } from '../transactions/transaction.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  // todo need to figure out correct way of using chart.js
+  income: number;
+  expense: number;
+  goal: number;
+
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+    scales: {
+      xAxes: [{
+        gridLines: {
+          drawBorder: false,
+        }
+      }],
+      yAxes: [{
+        display: false,
+        gridLines: {
+          drawBorder: false,
+        }
+      }],
+    },
+    tooltips: {
+      enabled: false
+    }
+  };
+
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = false;
+  public barChartPlugins = [];
+
+  public barChartData: ChartDataSets[] = [];
+
+  constructor(
+    private transactionService: TransactionService
+  ) { }
 
   ngOnInit() {
+    this.transactionService.getUserTransactions().subscribe(data => {
+      this.income = data.transactions.filter(trans => {
+        if (trans.credit) {
+          return trans;
+        }
+      }).reduce((accumulator, currentValue) => {
+        return accumulator + currentValue.credit;
+      }, 0);
+
+      this.expense = data.transactions.filter(trans => {
+        if (trans.debit) {
+          return trans;
+        }
+      }).reduce((accumulator, currentValue) => {
+        return accumulator + currentValue.debit;
+      }, 0);
+
+      this.goal = this.income - this.expense;
+
+      this.barChartData = [
+        { data: [this.income], label: 'Income' },
+        { data: [this.expense], label: 'Expense' }
+      ];
+    });
   }
 
 }
